f53695c01bd2f93662139a0b849fe8d9b4aebd59
RykuPrime <Dynasty7@outlook.com>
2022-05-26 17:23:10 -0400

    Initial commit

---
+++ src/main/java/com/example/ngoprojectbackend/NgoProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.ngoprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NgoProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NgoProjectBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/ngoprojectbackend/NgoProjectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ngoprojectbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NgoProjectBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
